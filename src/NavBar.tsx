2388cf2faaec2b31faef64ff7ead6da764293e38
michaelsainttt <michaelsaint2005@gmail.com>
2025-09-19 01:18:32 -0400

    finallllll

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -9,6 +9,8 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "./lib/supabaseClient";
+import { Link } from "react-router-dom";
+import logo from "./assets/dicerythmrank.png";
 
 
 export default function NavBar() {
@@ -40,29 +42,37 @@ export default function NavBar() {
   return (
  <div className="bg-black/50 p-8 position:sticky flex justify-between items-center w-full font-bold px-30">
   <div className = "flex space-x-3">
-    <img className = "size-10"src="public/dicerythmrank.png"></img>
+    <img className = "size-10"src={logo}></img>
     <h1 className="text-green-500 text-3xl font-bold">RhythmRank</h1>
   </div>
 
   <NavigationMenu>
-    <NavigationMenuList className="flex space-x-8">
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <a href="/" className="text-white hover:text-green-500 transition-colors hover:bg-gray-900">Home</a>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <a href="/submit" className="text-white hover:text-green-500 transition-colors hover:bg-gray-900">Submit Playlist</a>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <a href="/leaderboard" className="text-white hover:text-green-500 transition-colors hover:bg-gray-900">Leaderboard</a>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-    </NavigationMenuList>
-  </NavigationMenu>
+        <NavigationMenuList className="flex space-x-8">
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link to="/" className="text-white hover:text-green-500 transition-colors hover:bg-gray-900">
+                Home
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link to="/submit" className="text-white hover:text-green-500 transition-colors hover:bg-gray-900">
+                Submit Playlist
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link to="/leaderboard" className="text-white hover:text-green-500 transition-colors hover:bg-gray-900">
+                Leaderboard
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
   {session ? (
        <Button onClick = {handleLogout} className = "px-6 hover:text-green-500 transition-colors cursor-pointer">Logout</Button>
     ) : (
