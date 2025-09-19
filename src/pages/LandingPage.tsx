eeca5c5b2edaab1d50d6d06deb363bec94d48c1c
michaelsainttt <michaelsaint2005@gmail.com>
2025-09-19 01:49:40 -0400

    bugs

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -18,19 +18,24 @@ export default function LandingPage() {
         <div className="flex flex-col md:flex-row items-center justify-center h-full px-6 md:px-20 gap-8">
           <div className="flex-1 flex flex-col justify-center items-start text-center md:text-left">
             <TextGrab />
-            <Button className="mt-6 w-40 md:w-48 h-12 bg-green-700 hover:bg-green-600 transition-transform hover:scale-105 cursor-pointer" onClick = {() => navigate("/signIn") }>
+            <Button
+              className="mt-6 w-40 md:w-48 h-12 bg-green-700 hover:bg-green-600 transition-transform hover:scale-105 cursor-pointer"
+              onClick={() => navigate("/signIn")}
+            >
               Get Started
             </Button>
           </div>
-          <div className="flex-1 flex justify-center md:justify-end items-center -translate-y-16">
-            <PlayListCard
-              id = {1}
-              title="My Current Gym Playlist!"
-              description="Hard-hitting tracks to fuel your workout"
-              tags={["#GymWorkout", "#RapMusic", "#Motivational"]}
-              spotify_url="https://open.spotify.com/playlist/37i9dQZF1DWTl4y3vgJOXW?si=ebd5b56d3db84138"
-              initialLikes={237}
-            />
+          <div className="flex-1 flex justify-center md:justify-end items-start mt-8 md:mt-0">
+            <div className="w-full max-w-md md:max-w-lg lg:max-w-xl">
+              <PlayListCard
+                id={1}
+                title="My Current Gym Playlist!"
+                description="Hard-hitting tracks to fuel your workout"
+                tags={["#GymWorkout", "#RapMusic", "#Motivational"]}
+                spotify_url="https://open.spotify.com/playlist/37i9dQZF1DWTl4y3vgJOXW?si=ebd5b56d3db84138"
+                initialLikes={237}
+              />
+            </div>
           </div>
         </div>
       </div>
